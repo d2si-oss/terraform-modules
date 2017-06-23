c6d38f1973f22c5153e7e4b1765e021e10a8dae3
Laurent Bernaille <laurent.bernaille@gmail.com>
2017-06-23 16:35:24 +0200

    Fix calculation of AZ count

--- aws/helper/outputs.tf
+++ aws/helper/outputs.tf
@@ -1,3 +1,3 @@
 output "azs" {
-  value = "${slice(data.aws_availability_zones.available.names, 0, (var.azs_count == 0 ? length(data.aws_availability_zones.available.names) : min(var.azs_count,length(data.aws_availability_zones.available.names))) - 1)}"
+  value = "${slice(data.aws_availability_zones.available.names, 0, (var.azs_count == 0 ? length(data.aws_availability_zones.available.names) : min(var.azs_count,length(data.aws_availability_zones.available.names))) )}"
 }
