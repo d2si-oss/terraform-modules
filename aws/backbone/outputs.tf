75a09c6723898669f615d2e95fb99a69a6c0a333
Antoine Jacoutot <antoine.jacoutot@d2-si.eu>
2017-07-17 12:26:16 +0200

    Add a few useful outputs (more are missing).

--- aws/backbone/outputs.tf
+++ aws/backbone/outputs.tf
@@ -18,6 +18,27 @@
 #  ]
 #}
 
+output "private_subnets_cidr" {
+  description = "List of private subnets CIDR"
+  value       = ["${aws_subnet.private.*.cidr_block}"]
+}
+
+output "private_subnets_id" {
+  description = "List of private subnets id"
+  value       = ["${aws_subnet.private.*.id}"]
+}
+
+output "public_subnets_cidr" {
+  description = "List of public subnets CIDR"
+  value       = ["${aws_subnet.public.*.cidr_block}"]
+}
+
+output "public_subnets_id" {
+  description = "List of public subnets id"
+  value       = ["${aws_subnet.public.*.id}"]
+}
+
 output "vpc_id" {
-  value = "${aws_vpc.main.id}"
+  description = "VPC id"
+  value       = "${aws_vpc.main.id}"
 }
