321ddf56682c8a3050c0f2754e9c24bfee53f45f
Gauthier Wallet <gauthier.wallet@gmail.com>
2017-10-26 10:59:15 +0200

    Update main.tf

--- aws/backbone/main.tf
+++ aws/backbone/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  # Returns a list of availability zone names which length is the of the available zones
+  # Returns a list of availability zone names which length is of the available zones one
   # or the minimum between the user provided one & available zones length.
   # This ensures to always have a valid number of availability zone names.
   azs = "${slice(data.aws_availability_zones.available.names, 0, (var.azs_count == 0 
