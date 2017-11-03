236f71ecb046ae0ab3bf45fc2594282fd2f188ef
Ninir <gauthier.wallet@gmail.com>
2017-11-03 15:35:52 +0100

    Added variable-based subnetworks management

--- gcp/backbone/main.tf
+++ gcp/backbone/main.tf
@@ -5,102 +5,95 @@ provider "google" {
 
 resource "google_compute_network" "network" {
   name                    = "${var.network_name}"
-  auto_create_subnetworks = "false"
+  auto_create_subnetworks = false
 }
 
-resource "google_compute_subnetwork" "front" {
-  name                     = "front"
-  ip_cidr_range            = "10.0.0.0/24"
+resource "google_compute_subnetwork" "subnetworks" {
+  count                    = "${length(keys(var.subnetworks))}"
+  name                     = "${var.network_name}-${element(keys(var.subnetworks), count.index)}"
+  ip_cidr_range            = "${element(values(var.subnetworks), count.index)}"
   network                  = "${google_compute_network.network.self_link}"
   region                   = "${var.gcp_region}"
-  private_ip_google_access = true
+  private_ip_google_access = "${var.subnetwork_private_ip_google_access}"
 }
 
-resource "google_compute_subnetwork" "back" {
-  name                     = "back"
-  ip_cidr_range            = "10.0.1.0/24"
-  network                  = "${google_compute_network.network.self_link}"
-  region                   = "${var.gcp_region}"
-  private_ip_google_access = true
-}
+# resource "google_compute_address" "nat_gateway" {
+#   name = "nat-gateway"
+# }
 
-resource "google_compute_address" "nat_gateway" {
-  name = "nat-gateway"
-}
+# resource "google_compute_instance" "nat_gateway" {
+#   name         = "${var.network_name}-nat-gateway"
+#   machine_type = "n1-standard-1"
+#   zone         = "${var.gcp_region}-b"
 
-resource "google_compute_instance" "nat_gateway" {
-  name         = "nat-gateway"
-  machine_type = "n1-standard-1"
-  zone         = "${var.gcp_region}-b"
+#   can_ip_forward = true
 
-  can_ip_forward = true
+#   boot_disk {
+#     initialize_params {
+#       image = "debian-cloud/debian-9"
+#     }
+#   }
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
+#   network_interface {
+#     subnetwork = "${google_compute_subnetwork.front.self_link}"
 
-  network_interface {
-    subnetwork = "${google_compute_subnetwork.front.self_link}"
+#     access_config {
+#       nat_ip = "${google_compute_address.nat_gateway.address}"
+#     }
+#   }
 
-    access_config {
-      nat_ip = "${google_compute_address.nat_gateway.address}"
-    }
-  }
+#   service_account {
+#     scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+#   }
 
-  service_account {
-    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
-  }
+#   metadata_startup_script = <<EOF
+# echo 1 > /proc/sys/net/ipv4/ip_forward
+# iptables -t nat -A POSTROUTING -j MASQUERADE
+# EOF
 
-  metadata_startup_script = <<EOF
-echo 1 > /proc/sys/net/ipv4/ip_forward
-iptables -t nat -A POSTROUTING -j MASQUERADE
-EOF
+#   tags = ["nat-gateway"]
+# }
 
-  tags = ["nat-gateway"]
-}
+# resource "google_compute_route" "default_route" {
+#   name             = "to-nat-gateway"
+#   dest_range       = "0.0.0.0/0"
+#   network          = "${google_compute_network.network.self_link}"
+#   next_hop_instance = "nat-gateway"
+#   next_hop_instance_zone = "${var.gcp_region}-b"
+#   tags             = ["nated"]
+#   priority         = 100
+# }
 
-resource "google_compute_route" "default_route" {
-  name             = "to-nat-gateway"
-  dest_range       = "0.0.0.0/0"
-  network          = "${google_compute_network.network.self_link}"
-  next_hop_instance = "nat-gateway"
-  next_hop_instance_zone = "${var.gcp_region}-b"
-  tags             = ["nated"]
-  priority         = 100
-}
+# resource "google_compute_firewall" "local" {
+#   name    = "local"
+#   network = "${google_compute_network.network.self_link}"
 
-resource "google_compute_firewall" "local" {
-  name    = "local"
-  network = "${google_compute_network.network.self_link}"
+#   allow {
+#     protocol = "icmp"
+#   }
 
-  allow {
-    protocol = "icmp"
-  }
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["22"]
+#   }
 
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
+#   source_ranges = ["10.0.0.0/16"]
+# }
 
-  source_ranges = ["10.0.0.0/16"]
-}
+# resource "google_compute_firewall" "nat_to_gateway" {
+#   name    = "nat-to-gateway"
+#   network = "${google_compute_network.network.self_link}"
 
-resource "google_compute_firewall" "nat_to_gateway" {
-  name    = "nat-to-gateway"
-  network = "${google_compute_network.network.self_link}"
+#   allow {
+#     protocol = "icmp"
+#   }
 
-  allow {
-    protocol = "icmp"
-  }
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["80", "443"]
+#   }
 
-  allow {
-    protocol = "tcp"
-    ports    = ["80", "443"]
-  }
+#   source_tags = ["nated"]
 
-  source_tags = ["nated"]
-
-  target_tags = ["nat-gateway"]
-}
+#   target_tags = ["nat-gateway"]
+# }
--- gcp/backbone/variables.tf
+++ gcp/backbone/variables.tf
@@ -4,4 +4,8 @@ variable "gcp_region" {
   default = "europe-west1"
 }
 
+variable "subnetwork_private_ip_google_access" {
+  default = true
+}
+
 variable "network_name" {}
