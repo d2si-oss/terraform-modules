29f7ccd1c336aa4c4ee071bc82d5d24c66fd8315
Marc Falzon <marc.falzon@d2-si.eu>
2017-11-03 13:50:31 +0100

    Variabilize network name

--- gcp/backbone/main.tf
+++ gcp/backbone/main.tf
@@ -4,7 +4,7 @@ provider "google" {
 }
 
 resource "google_compute_network" "network" {
-  name                    = "plop"
+  name                    = "${var.network_name}"
   auto_create_subnetworks = "false"
 }
 
--- gcp/backbone/variables.tf
+++ gcp/backbone/variables.tf
@@ -3,3 +3,5 @@ variable "gcp_project" {}
 variable "gcp_region" {
   default = "europe-west1"
 }
+
+variable "network_name" {}
